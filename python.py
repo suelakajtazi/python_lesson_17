d7b0b60ac7704c924070c70432d66d28db76f6c9
suelakajtazi <kajtazisuela1@gmail.com>
2025-09-11 20:35:51 +0200

    hiiii

--- python.py
+++ python.py
@@ -30,3 +30,56 @@ with st.container():
     st.write("aeiofhwwasdmk efiohfuiewfcds")
 
 st.write("aeiofhwwasdmk efiohfuiewfcds")
+
+
+st.sidebar.header("Sidebar")
+st.sidebar.write("this is sidebar")
+st.sidebar.selectbox("Chose",["home","contact us","about us"])
+st.sidebar.radio("Chose",["home","contact us","about us"])
+
+with st.form("myform",clear_on_submit=True):
+        name = st.text_input("enter ur name")
+        surname = st.text_input("surname")
+        age = st.slider("age")
+        email = st.text_input("email")
+        terms = st.checkbox("agree")
+        submit_button = st.form_submit_button("submit")
+
+if submit_button:
+       st.write([name])
+       st.write([surname])
+       st.write([age])
+       st.write([email])
+       
+       if terms:
+            st.write("u agreed")
+       else:
+            st.write("u didnt agree")
+
+
+tab1,tab2,tab3 = st.tabs(["home","about","contact"])
+
+with tab1:
+     st.header("tab1")
+
+with tab2:
+     with st.form("urform",clear_on_submit=True):
+        name = st.text_input("enter ur name")
+        surname = st.text_input("surname")
+        age = st.slider("age")
+        email = st.text_input("email")
+        terms = st.checkbox("agree")
+        submit_button = st.form_submit_button("submit")
+
+if submit_button:
+       st.write([name])
+       st.write([surname])
+       st.write([age])
+       st.write([email])
+       
+       if terms:
+            st.write("u agreed")
+       else:
+            st.write("u didnt agree")
+with tab3:
+     st.header("hiiiiiiiiiiiiii")
